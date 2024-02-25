4ebf74a04d45dc863b216e2fc9728e60a99abc62
Kyler <59854022+KylerOlsen@users.noreply.github.com>
2024-02-24 23:45:03 -0700

    Added printing signed integers

--- emulator/devices.py
+++ emulator/devices.py
@@ -7,7 +7,12 @@ from .emulator import Device
 class tty(Device):
 
     def __setitem__(self, index: int, value: int):
-        if index & 0xf == 0xe:
+        if index & 0xf == 0xd:
+            if value & 0x800:
+                print(((~(value & 0x7FF)) + 1) * -1)
+            else:
+                print(value)
+        elif index & 0xf == 0xe:
             print(value)
         elif index & 0xf == 0xf:
             print(chr(value), end='')
