c655bb898273cca124062be6430abe4560203079
Kyler <59854022+KylerOlsen@users.noreply.github.com>
2024-03-08 14:06:47 -0700

    Added character input to tty machine

--- pytd12dk/emulator/devices.py
+++ pytd12dk/emulator/devices.py
@@ -4,7 +4,32 @@
 from .emulator import Device
 
 
+try:
+    # Windows
+    import msvcrt
+    def getch() -> int:
+        return msvcrt.getch()[0]
+except ImportError:
+    # Unix
+    import sys
+    import tty, termios
+    def getch() -> int:
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1).encode('utf-8')[0]
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+
 class tty(Device):
+
+    def __getitem__(self, index: int) -> int:
+        if index & 0xf == 0xd: return 0
+        elif index & 0xf == 0xe: return 0
+        elif index & 0xf == 0xf: return getch()
+        else: return 0
 
     def __setitem__(self, index: int, value: int):
         if index & 0xf == 0xd:
